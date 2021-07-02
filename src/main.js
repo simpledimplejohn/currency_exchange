601d832bb948b722013c342c5044c6e504affefc
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-02 10:23:46 -0700

    restart of currency exchange project with correct spelling

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import './css/styles.css';
+
+$(".test").html("test")
+console.log(`The API key is: ${process.env.API_KEY}`)
+
+
+
