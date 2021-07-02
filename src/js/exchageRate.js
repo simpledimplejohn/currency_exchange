7a11f4ab9075b3c72c0ef914a89fdbbb4825fad3
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-02 11:49:15 -0700

    make api call attempt

--- src/js/exchageRate.js
+++ src/js/exchageRate.js
@@ -1,5 +1,5 @@
 export default class ExchangeRate {
-  static async getCountryRate(countryCode) {
+  static async getCountryRate() {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       if (!response.ok) {
--- src/main.js
+++ src/main.js
@@ -2,11 +2,29 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import './css/styles.css';
+import ExchangeRate from './js/exchageRate';
 
-import ExchangeRate froom './js/exchangerate'
 
 $(".test").html(`${process.env.API_KEY}`)
 console.log(`The API key is: ${process.env.API_KEY}`)
 
 
+
+async function makeApiCall(choice) {
+  try{
+    console.log("choice in makeApiCall: ", choice);
+    const response = await ExchangeRate(choice);
+    console.log("makeApiCall response: ", response);
+    
+  }
+  catch(err) {
+    console.log("await failed error: ", err);
+  }
+}
+
+$(document).ready(function() {
+  console.log("document.read works!");
+  let choice = "AUD"
+  console.log("calling makeApiCall with choice: ", makeApiCall(choice));
+})
 
