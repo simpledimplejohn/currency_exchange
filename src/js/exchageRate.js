495c6cdaf78051c00d9143599447ccad1392083f
Simple Dimple John <simpledimplejohn@gmail.com>
2021-07-03 12:38:56 -0700

    remove consolelogs

--- src/js/exchageRate.js
+++ src/js/exchageRate.js
@@ -3,13 +3,10 @@ export default class ExchangeRate {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${choice}`);
       if (!response.ok) {
-        console.log("throw error:", response.status)
         throw Error(response.status);
       }
       return response.json();
     } catch(error) {
-      console.log("catch", error)
       return error;
     }
   }
-}
--- src/main.js
+++ src/main.js
@@ -1,41 +1,27 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import './css/styles.css';
 import ExchangeRate from './js/exchageRate';
-
 
 function clearFields() {
   $('.showRate').html("");
   $('.showExchange').text("");
   $('.showErrors').text("");
-
 }
 
-
 function getElements(response, usdInput) {
-  if(response.result) {
+  if(response.status) {
     $('.showRate').html(`<p>Your conversion rate is: ${response.conversion_rates["USD"]}</P>`)
-    console.log("sent to DOM", response.conversion_rates["USD"])
     let newValue = Math.floor(usdInput / response.conversion_rates["USD"])
     $('.showExchange').text(`That means ${usdInput} dollars is worth ${newValue} in ${response.base_code} currency`)
-    console.log("dollars moved",usdInput)
-    console.log("converted", newValue)
   } else {
     $('.showErrors').text(`There was an error: ${response}`)
-    console.log("getElements error:", response)
-
   }
 }
 
-
 async function makeApiCall(choice, usdInput) {
-
-  console.log("choice in makeApiCall: ", choice);
   const response = await ExchangeRate.getCountryRate(choice);
-  console.log("makeApiCall sends to getElements:", response);
   getElements(response, usdInput);
-
 }
 
 $(document).ready(function() {
@@ -43,13 +29,8 @@ $(document).ready(function() {
     event.preventDefault();
     let choice = $('#countryCode').val();
     let usdInput = $('#usdInput').val();
-    console.log("usdInput:",usdInput)
-    //console.log("form input: ",choice);
     clearFields();
     makeApiCall(choice, usdInput);
-    //$(".showRate").text(makeApiCall(choice));
-    //console.log("what's getting sent from form:", makeApiCall(choice, usdInput));
-
   })
 })
 
